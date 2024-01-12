ca3aa4207276f46c61585b267c80a6602996a5b0
farrisismatiteach <farris.ismati.teach@gmail.com>
2024-01-12 16:48:47 -0500

    gonna make this d3 line work

--- src/components/VisionForm/VisionDemo/Sections/LocationSection/GrowthCurveGraph.tsx
+++ src/components/VisionForm/VisionDemo/Sections/LocationSection/GrowthCurveGraph.tsx
@@ -1,9 +1,8 @@
 import { createGrowthCurve } from "@/components/VisionForm/utils/growthCurve";
-import { pxStringToNum, spacer320 } from "@/styles/sizes";
-import { getLineColor } from "@/types/Charts/Legend";
+import LineChart from "@/components/common/Charts/LineChart";
+import { ChartFilterEnum } from "@/types/Charts/Filter";
 import { CurveType } from "@/types/VisionForm/locationSection";
 import { useMemo } from "react";
-import { Label, Line, LineChart, XAxis, YAxis } from "recharts";
 
 export interface GrowthCurveGraphProps {
 	/**
@@ -37,41 +36,54 @@ const GrowthCurveGraph = ({
 	};
 
 	return (
-		// RECHARTS
 		<LineChart
-			width={pxStringToNum(spacer320)}
-			height={pxStringToNum(spacer320)}
 			data={curveDataPoints}
-		>
-			<Line
-				type="monotone"
-				dataKey="uv"
-				strokeWidth={2}
-				stroke={getLineColor("uv")}
-				dot={false}
-			/>
-			<XAxis dataKey="name">
-				<Label
-					style={{
-						textAnchor: "middle",
-						fontSize: "80%",
-					}}
-					angle={0}
-					value={"Time"}
-				/>
-			</XAxis>
-			<YAxis>
-				<Label
-					style={{
-						textAnchor: "middle",
-						fontSize: "80%",
-					}}
-					angle={270}
-					value={"Growth Percentage"}
-				/>
-			</YAxis>
-		</LineChart>
+			isDataLoaded={curveDataPoints.length > 0}
+			initialLineChartData={initialLineChartData}
+			xField="date"
+			height={320}
+			width={400}
+			margin={{ top: 10, right: 20, bottom: 20, left: 20 }}
+			filter={ChartFilterEnum.Month}
+		/>
 	);
 };
 
 export default GrowthCurveGraph;
+
+// import { Label, Line, LineChart, XAxis, YAxis } from "recharts";
+
+// 		// RECHARTS
+// 		<LineChart
+// 			width={pxStringToNum(spacer320)}
+// 			height={pxStringToNum(spacer320)}
+// 			data={curveDataPoints}
+// 		>
+// 			<Line
+// 				type="monotone"
+// 				dataKey="uv"
+// 				strokeWidth={2}
+// 				stroke={getLineColor("uv")}
+// 				dot={false}
+// 			/>
+// 			<XAxis dataKey="name">
+// 				<Label
+// 					style={{
+// 						textAnchor: "middle",
+// 						fontSize: "80%",
+// 					}}
+// 					angle={0}
+// 					value={"Time"}
+// 				/>
+// 			</XAxis>
+// 			<YAxis>
+// 				<Label
+// 					style={{
+// 						textAnchor: "middle",
+// 						fontSize: "80%",
+// 					}}
+// 					angle={270}
+// 					value={"Growth Percentage"}
+// 				/>
+// 			</YAxis>
+// 		</LineChart>
